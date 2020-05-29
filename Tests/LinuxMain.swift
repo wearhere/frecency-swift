f94866e637eb1e56629782dfcb70d03317157d7c
Jeff Wear <jeff@mixmax.com>
2020-05-29 16:22:59 -0700

    Realize how Linux setup is supposed to work (I think)
    Still haven’t run these.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,7 +4,7 @@ import Quick
 @testable import FrecencyTests
 
 QCKMain([
-    FrecencySelectionTests.self,
-    FrecencySortingTests.self,
-    StringIsSubQueryTests.self
+    FrecencySelectionSpec.self,
+    FrecencySortingSpec.self,
+    StringIsSubQuerySpec.self
 ])
